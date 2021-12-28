bde5d268df343280014bf647d68f70271cfdfebc
LeonardoBrizolla <leonardobrizollars@gmail.com>
2021-12-28 18:38:00 -0300

    Create components title, button, wrap

---
+++ src/components/button-search/index.js
@@ -0,0 +1,6 @@
+const $buttonSearch = document.createElement('button');
+$buttonSearch.classList.add('btn');
+$buttonSearch.classList.add('btn-primary');
+$buttonSearch.textContent = 'Buscar';
+
+$wrapCards.insertBefore($buttonSearch, null);
---
+++ src/components/title/index.js
@@ -0,0 +1,7 @@
+function createTitle() {
+  const $title = `
+<h1>PokeLab Busca</h1>
+`;
+
+  $wrapCards.insertAdjacentHTML('afterbegin', $title);
+}
