f5025ecc6f940f9325bc5493e65c8e96acfdff93
LeonardoBrizolla <leonardobrizollars@gmail.com>
2022-01-03 13:48:10 -0300

    Add bgc selection + refactoring input data att

--- src/components/input/index.js
+++ src/components/input/index.js
@@ -10,20 +10,25 @@ const createInput = ({
   const $head = document.querySelector('head');
   const $style = document.createElement('style');
   $style.textContent = `
+  .input-pokelab.form-control {
+    background-color: #383a59;
+  }
+
+  .input-pokelab.form-control:focus {
+    background-color: #383a59;
+  }
+
   .input-pokelab.-search {
     border: solid 2px #bd93f9;
   }
 
   .input-pokelab.-search.form-control {
-    background-color: #383a59;
     color: #bd93f9;
   }
 
   .input-pokelab.-search.form-control:focus {
     color: #bd93f9;
-    background-color: #383a59;
     border-color: #bd93f9;
-    outline: 0;
     box-shadow: 0 0 0 0.25rem rgba(189, 147, 249, 0.25);
   }
 
@@ -32,15 +37,12 @@ const createInput = ({
   }
 
   .input-pokelab.-newsletter.form-control {
-    background-color: #383a59;
     color: #50fa7b;
   }
 
   .input-pokelab.-newsletter.form-control:focus {
     color: #50fa7b;
-    background-color: #383a59;
     border-color: #50fa7b;
-    outline: 0;
     box-shadow: 0 0 0 0.25rem rgba(80, 250, 123, 0.5);
   }
 
