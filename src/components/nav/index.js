6910a9109c542520b889af114a2e8749b3f3edc7
LeonardoBrizolla <leonardobrizollars@gmail.com>
2022-01-04 13:48:10 -0300

    Update hover links nav new color

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -9,13 +9,13 @@ const createNavigation = () => {
       background-color: #282a36;
     }
 
-    .navigation-pokelab .title {
+    .navigation-pokelab .-title {
       font-family: 'Pushster', cursive;
       font-size: 1.7em;
     }
 
-    .navigation-pokelab .nav-link {
-      font-family: 'Roboto Mono', monospace;
+    .navigation-pokelab .-title:hover {
+      color: #bd93f9;
     }
 
     .navigation-pokelab .nav-item {
@@ -27,10 +27,21 @@ const createNavigation = () => {
       border-radius: 9px;
       transition: opacity 200ms linear;
       cursor: pointer;
+      color: black;
     }
 
     .navigation-pokelab .highlighted-pokelab:hover {
-      opacity: 0.8;
+      opacity: 0.7;
+    }
+
+    .navigation-pokelab .navbar-nav .nav-link {
+      font-family: 'Roboto Mono', monospace;
+      color: #f8f8f2;
+      transition: border-bottom 100ms linear;
+    }
+
+    .navigation-pokelab .navbar-nav .nav-link:hover {
+      color: #bd93f9;
     }
 
     .navigation-pokelab .nav-link.-pokelab {
@@ -57,45 +68,44 @@ const createNavigation = () => {
       }
     }
   `;
-
   $head.insertBefore($style, null);
 
   return `
-  <nav class="navbar navbar-expand-lg navbar-dark top-bar fixed-top navigation-pokelab">
-    <div class="container-fluid">
-      <a class="navbar-brand h1 title" href="/">PokéLab</a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse justify-content-end collapse-pokelab" id="navbarNav">
-        <ul class="navbar-nav fs-5">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/">Buscar</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="../../../about.html">Sobre</a>
-          </li>
-          <li class="nav-item highlighted-pokelab">
-            <a
-              class="nav-link -pokelab"
-              data-nav="btn-newsletter"
-              data-bs-toggle="modal"
-              data-bs-target="#newsletterModal"
-              onClick="handleClickNewsletter()"
-              >
-                News Letter &#10024
-              </a>
-          </li>
-        </ul>
+    <nav class="navbar navbar-expand-lg navbar-dark top-bar fixed-top navigation-pokelab">
+      <div class="container-fluid">
+        <a class="navbar-brand h1 -title" href="/">PokéLab</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end collapse-pokelab" id="navbarNav">
+          <ul class="navbar-nav fs-5">
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="/">Buscar</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="../../../about.html">Sobre</a>
+            </li>
+            <li class="nav-item highlighted-pokelab">
+              <a
+                class="nav-link -pokelab"
+                data-nav="btn-newsletter"
+                data-bs-toggle="modal"
+                data-bs-target="#newsletterModal"
+                onClick="handleClickNewsletter()"
+                >
+                  News Letter &#10024
+                </a>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
-`;
+    </nav>
+  `;
 };
